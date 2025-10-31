3f9792b9b144a79b96a303f99b67c762ffbdc71a
Adre <dreenation21@gmail.com>
2025-10-31 11:37:18 +0700

    feat: update provider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\BukuTamu;
-use App\Models\PegawaiModel;
-use App\Models\SiswaModel;
-use App\Models\Orangtua;
-use App\Models\JabatanModel;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\ServiceProvider;
@@ -26,39 +21,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Mengirimkan total pegawai & total buku tamu ke semua view yang menggunakan 'layouts.admin'
-        // View::composer('admin.v_admin', function ($view) {
-        //     $view->with([
-        //         'totalPegawai' => PegawaiModel::count(),
-        //         'totalBukuTamu' => BukuTamu::count(),
-        //     ]);
-        // });
-
-        // Mengirimkan total pegawai & total buku tamu ke seluruh view yang ada di projek ini
-        // View::share('totalPegawai', PegawaiModel::count());
-        // View::share('totalBukuTamu', BukuTamu::count());
-        // View::share('totalSiswa', SiswaModel::count());
-        // View::share('totalOrangtua', Orangtua::count());
-        // View::share('totalJabatan', JabatanModel::count());
-
-        if (Schema::hasTable('tbl_pegawai')) {
-            View::share('totalPegawai', PegawaiModel::count());
-        }
-
-        if (Schema::hasTable('tbl_bukutamu')) {
-            View::share('totalBukuTamu', BukuTamu::count());
-        }
-
-        if (Schema::hasTable('tbl_siswa')) {
-            View::share('totalSiswa', SiswaModel::count());
-        }
-
-        if (Schema::hasTable('tbl_orangtua')) {
-            View::share('totalOrangtua', Orangtua::count());
-        }
-
-        if (Schema::hasTable('tbl_jabatan')) {
-            View::share('totalJabatan', JabatanModel::count());
-        }
+        //
     }
 }
