1a005fb268f2f72526ecdaaf81d5d4982383f2ef
Adre <dreenation21@gmail.com>
2025-10-31 11:41:53 +0700

    feat: update migrations

---
+++ database/migrations/2025_10_31_113958_create_jabatans_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('jabatans', function (Blueprint $table) {
+            $table->id();
+            $table->string('nip')->unique()->nullable();
+            $table->string('nama_pegawai');
+            $table->foreignId('jabatan_id')->constrained('jabatans')->onDelete('cascade');
+            $table->string('kontak')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('jabatans');
+    }
+};
---
+++ database/migrations/2025_10_31_114101_create_buku_tamus_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buku_tamus', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama');
+            $table->string('role');
+            $table->string('instansi')->nullable();
+            $table->text('alamat');
+            $table->string('kontak')->nullable();
+            $table->foreignId('siswa_id')->nullable()->constrained('siswas')->onDelete('set null');
+            $table->foreignId('jabatan_id')->nullable()->constrained('jabatans')->onDelete('set null');
+            $table->foreignId('pegawai_id')->nullable()->constrained('pegawais')->onDelete('set null');
+            $table->text('keperluan');
+            $table->string('foto_tamu')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buku_tamus');
+    }
+};
