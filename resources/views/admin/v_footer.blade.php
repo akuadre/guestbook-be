43f4ba353e03b5782b834256370ac1bcbc6bd600
Adre <dreenation21@gmail.com>
2025-10-31 10:45:02 +0700

    feat: removing useless file

--- resources/views/admin/v_footer.blade.php
+++
@@ -1,5 +0,0 @@
-<footer class="bg-white shadow-inner py-4">
-    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 text-center text-gray-600 text-sm">
-        &copy; {{ date('Y') }} School Guestbook. All rights reserved.
-    </div>
-</footer>
